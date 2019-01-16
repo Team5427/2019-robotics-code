b56ef8189c3feb6ee7cbe45a70913f3dbda7d929
Blake Romero <blake.romero724@gmail.com>
2019-01-15 21:16:10 -0600

    Added most future buttons to OI and Config, no working commands yet.

--- src/main/java/org/usfirst/frc/team5427/robot/OI.java
+++ src/main/java/org/usfirst/frc/team5427/robot/OI.java
@@ -26,17 +26,41 @@ public class OI {
 
 	public Joystick joy1;
 
-	Button intakeIn;
-
 	Button intakeOut;
+	Button intakeIn;
+	Button elbowDown;
+	Button elevatorDown;
+	Button elbowUp;
+	Button elevatorUp;
+	Button hatchEject;
+	Button levelThreeClimb;
+	Button wristUp;
+	Button wristDown;
 
 	public OI() {
 		joy1 = new Joystick(Config.JOYSTICK_PORT);
-		intakeIn = new JoystickButton(joy1,Config.BUTTON_INTAKE_IN);
+
 		intakeOut = new JoystickButton(joy1,Config.BUTTON_INTAKE_OUT);
-	
-		intakeIn.whenPressed(new IntakeCargo());
+		intakeIn = new JoystickButton(joy1,Config.BUTTON_INTAKE_IN);
+		elbowDown = new JoystickButton(joy1,Config.BUTTON_ELBOW_DOWN);
+		elevatorDown = new JoystickButton(joy1, Config.BUTTON_ELEVATOR_DOWN);
+		elbowUp = new JoystickButton(joy1,Config.BUTTON_ELBOW_UP);
+		elevatorUp = new JoystickButton(joy1,Config.BUTTON_ELEVATOR_UP);
+		hatchEject = new JoystickButton(joy1,Config.BUTTON_HATCH_EJECT);
+		levelThreeClimb = new JoystickButton(joy1,Config.BUTTON_LEVEL_THREE_CLIMB);
+		wristUp = new JoystickButton(joy1,Config.BUTTON_WRIST_UP);
+		wristDown = new JoystickButton(joy1,Config.BUTTON_WRIST_DOWN);
+
 		intakeOut.whenPressed(new EjectCargo());
+		intakeIn.whenPressed(new IntakeCargo());
+		// elbowDown.whenPressed(new ElbowDown());
+		// elevatorDown.whenPressed(new ElevatorDown());
+		// elbowUp.whenPressed(new ElbowUp());
+		// elevatorUp.whenPressed(new ElevatorUp());
+		// hatchEject.whenPressed(new EjectHatch());
+		// levelThreeClimb.whenPressed(new LevelThreeClimb());
+		// wristUp.whenPressed(new WristUp());
+		// wristDown.whenPressed(new WristDown());
 	}
 
 	/**
