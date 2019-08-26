00057287ac8a0860b36a8bb9976dbc88904d854b
Abinavraj5427 <abinavraj10@gmail.com>
2019-08-25 22:48:30 -0500

    Adjusted spacing for Arm.java

--- src/main/java/org/usfirst/frc/team5427/robot/subsystems/Arm.java
+++ src/main/java/org/usfirst/frc/team5427/robot/subsystems/Arm.java
@@ -7,29 +7,22 @@ import org.usfirst.frc.team5427.util.Config;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * This class creates a Subsystem for the arm. 
- */
-public class Arm extends Subsystem
-{
-    //237 - 159
+//237 - 159
+
+/**This class creates a Subsystem for the arm. */
+public class Arm extends Subsystem{
     
-    /**
-     * Speed controller for the arm motor. 
-     */
+     /**Speed controller for the arm motor. */
     private SpeedController armMotor;
 
-    /**
-     * Value to store the angle of the arm. 
-     */
-    public double armAngle;
+    /** Value to store the angle of the arm.*/
+    private double armAngle;
 
     /**
      * Constructor for the arm subsystem that initializes the arm speed controller. 
      * @param armMotor the speed controller that controls the arm motor. 
      */
-    public Arm(SpeedController armMotor)
-    {
+    public Arm(SpeedController armMotor){
         this.armMotor = armMotor;
     }
 
@@ -38,48 +31,28 @@ public class Arm extends Subsystem
      * at the limits, the speed is set to 0. 
      * @param armSpeed The received speed that the arm needs to move at. 
      */
-    public void moveArm(double armSpeed)
-    {
+    public void moveArm(double armSpeed){
         //if the arm is not at the limits
-        if((armSpeed > 0 && Robot.getArmPot().get() >= Config.ARM_LIMIT_TOP) 
+        if((armSpeed > 0 && Robot.getArmPot().get() >= Config.ARM_LIMIT_TOP)
         || (armSpeed < 0 && Robot.getArmPot().get() <= Config.ARM_LIMIT_BOTTOM))
-            armMotor.set(armSpeed);
-        else //if the arm is at or beyond the limits
-            armMotor.set(0);
+            armMotor.set(armSpeed); 
+        else armMotor.set(0);  //if the arm is at or beyond the limits
     }
 
-    /**
-     * Sets the motor speed of the arm to 0. 
-     */
-    public void stop()
-    {
+    /** Sets the motor speed of the arm to 0.  */
+    public void stop(){
         armMotor.set(0);
     }
 
-    /**
-     * Required when extending Subsystem. In this case, the arm has no default command
-     */
+
+
+    /** Sets the arm's angle to given angle.  @param angle given angle for arm. */
+    public void setArmAngle(double angle){this.armAngle = angle;}
+
+    /** Returns the arm's angle.  @return the angle of the arm. */
+    public double getArmAngle(){return armAngle;}
+
+    /** Required when extending Subsystem. In this case, the arm has no default command*/
     @Override
-    protected void initDefaultCommand()
-    {
-
-    }
-
-    /**
-     * Sets the angle of the arm based on the received value. 
-     * @param angle the received value of the arm. 
-     */
-    public void setArmAngle(double angle)
-    {
-        this.armAngle = angle;
-    }
-
-    /**
-     * Gives the angle of the arm. 
-     * @return the angle of the arm. 
-     */
-    public double getArmAngle()
-    {
-        return armAngle;
-    }
+    protected void initDefaultCommand(){}
 }
