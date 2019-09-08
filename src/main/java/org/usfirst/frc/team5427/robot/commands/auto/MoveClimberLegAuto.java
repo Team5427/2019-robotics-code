fdd11a19e66d63669094c3e313bfca4cbfc5e99c
Vatsal Mistry <vatsalmistry4796@gmail.com>
2019-09-08 18:47:44 -0500

    documentation added for auto commands

--- src/main/java/org/usfirst/frc/team5427/robot/commands/auto/MoveClimberLegAuto.java
+++ src/main/java/org/usfirst/frc/team5427/robot/commands/auto/MoveClimberLegAuto.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class MoveClimberLegAuto extends Command
 {
- 
+    //declares and initializes any required variables
     private BigDecimal speed = new BigDecimal("0");
     private BigDecimal leg = new BigDecimal("0");
     private BigDecimal startLeg = new BigDecimal("0");;
@@ -19,6 +19,8 @@ public class MoveClimberLegAuto extends Command
     private BigDecimal goalLeg = new BigDecimal("0");
     private BigDecimal climbPot = new BigDecimal("0");
 
+
+    //recieves and sets a goal leg position
     public MoveClimberLegAuto(BigDecimal goalLeg)
     {
         requires(Robot.getClimberLeg());
@@ -28,45 +30,59 @@ public class MoveClimberLegAuto extends Command
     @Override
     protected void initialize()
     {
-        
+        //sets beginning position of leg to current encoder value
         startLeg = BigDecimal.valueOf(Robot.getClimbEnc().get());
 
-        if(this.goalLeg.compareTo(startLeg)>0) {
+        //decides if the leg should move up or down to reach desired position
+        if(this.goalLeg.compareTo(startLeg)>0) 
+        {
+            //sets leg to move at a speed downwards and records the difference between the end and start position
             this.speed = BigDecimal.valueOf(Config.CLIMBER_LEG_SPEED_DOWN);
             this.leg = this.goalLeg.subtract(startLeg).abs();
         }
-        else if(this.goalLeg.compareTo(startLeg)<0) {
+        else if(this.goalLeg.compareTo(startLeg)<0) 
+        {
+            //sets leg to move at a speed upwards and the records difference between the end and start position
             this.speed = BigDecimal.valueOf(Config.CLIMBER_LEG_SPEED_UP);
             this.leg = this.goalLeg.subtract(startLeg).abs();            
         }
         this.setInterruptible(true);
 
+        //actually sets the leg to move based on previously decided speed
         Robot.getClimberLeg().setSpeed(this.speed.doubleValue());
     }
 
     @Override
     protected void execute()
     {
+        //called repeatedly until command is finished or stopped
         Robot.getClimberLeg().setSpeed(this.speed.doubleValue());
     }
 
     @Override
     protected boolean isFinished()
     {
+        //records current value of the climber leg encoder
         climbPot = BigDecimal.valueOf(Robot.getClimbEnc().get());
 
-        if(climbPot.subtract(previous_leg_enc).abs().compareTo(new BigDecimal("2")) <=0) {
+        //checks if the differnce between the current encoder and previous encoder value(last time the method was called) is less than 2
+        if(climbPot.subtract(previous_leg_enc).abs().compareTo(new BigDecimal("2")) <=0) 
+        {
+            //sets the leg difference to the beggining position minus the current position
             endLegDifference = (startLeg.subtract(climbPot)).abs();
         }
         
+        //sets previous location to current location for next time the method is called
         previous_leg_enc = climbPot;
 
+        //returns true if the difference between the current leg difference and total difference is more than zero, checks if the leg moved the amount it needed to move
         return endLegDifference.compareTo(this.leg)>=0;
     }
 
     @Override
     protected void end()
     {
+        //sets velocity of climber leg to zero
         Robot.getClimberLeg().stop();
     }
 }
--- src/main/java/org/usfirst/frc/team5427/robot/commands/auto/RotateArmAuto.java
+++ src/main/java/org/usfirst/frc/team5427/robot/commands/auto/RotateArmAuto.java
@@ -14,6 +14,7 @@ public class RotateArmAuto extends Command
     //elevator: 390 counts/sec
     //elevator: 502 counts/sec
 
+    //declares and initializes any required variables
     public BigDecimal speed = new BigDecimal("0");
     public BigDecimal angle = new BigDecimal("0");
     public BigDecimal startArm = new BigDecimal("0");
@@ -21,6 +22,7 @@ public class RotateArmAuto extends Command
     public BigDecimal goalAngle = new BigDecimal("0");
     public BigDecimal previous_arm_pot = new BigDecimal("0");
 
+    //recieves and sets a goal angle position
     public RotateArmAuto(BigDecimal goalAngle)
     {
         requires(Robot.getArm());
@@ -31,31 +33,35 @@ public class RotateArmAuto extends Command
     @Override
     protected void initialize()
     {
-        BigDecimal armPotValue = BigDecimal.valueOf(Robot.getArmPot().get());
+        //sets beginning position of arm to current potentiometer value
+        BigDecimal startArm = BigDecimal.valueOf(Robot.getArmPot().get());
    
-        if(this.goalAngle.compareTo(armPotValue)<0) {
+        //decides if the arm should move up or down to reach desired position
+        if(this.goalAngle.compareTo(startArm)<0) 
+        {
+            //sets arm to move at a speed upwards and records the difference between the end and start position while accounting for the offset
             this.speed = BigDecimal.valueOf(Config.ARM_SPEED_UP_AUTO);
-            this.angle = (this.goalAngle.subtract(armPotValue).abs()).subtract(BigDecimal.valueOf(Config.angleOffsetUp_Arm));
+            this.angle = (this.goalAngle.subtract(startArm).abs()).subtract(BigDecimal.valueOf(Config.angleOffsetUp_Arm));
         }
-        else if(this.goalAngle.compareTo(armPotValue)>0) {
+        else if(this.goalAngle.compareTo(startArm)>0) 
+        {
+            //sets arm to move at a speed downwards and records the difference between the end and start position while accounting for the offset
             this.speed = BigDecimal.valueOf(Config.ARM_SPEED_DOWN_AUTO);
-            this.angle = (this.goalAngle.subtract(armPotValue).abs()).subtract(BigDecimal.valueOf(Config.angleOffsetDown_Arm));           
+            this.angle = (this.goalAngle.subtract(startArm).abs()).subtract(BigDecimal.valueOf(Config.angleOffsetDown_Arm));           
         }
 
      
 
         this.setInterruptible(true);
-        startArm = armPotValue;
 
-
-        endAngleDifference = new BigDecimal("0");
-
+        //actually sets the arm to move based on previously decided speed
         Robot.getArm().moveArm(this.speed.doubleValue());
     }
 
     @Override
     protected void execute()
     {
+        //called repeatedly until command is finished or stopped
         Robot.getArm().moveArm(this.speed.doubleValue());
     }
     
@@ -63,20 +69,27 @@ public class RotateArmAuto extends Command
     @Override
     protected boolean isFinished()
     {
+        //records current value of the arm potentiometer
         BigDecimal arm_pot = BigDecimal.valueOf(Robot.getArmPot().get());
 
-        if((arm_pot.subtract(previous_arm_pot)).abs().compareTo(new BigDecimal("2"))<=0) {
+        //checks if the differnce between the current pot and previous pot value(last time the method was called) is less than 2
+        if((arm_pot.subtract(previous_arm_pot)).abs().compareTo(new BigDecimal("2"))<=0) 
+        {
+            //sets the angle difference to the beggining position minus the current position
             endAngleDifference = startArm.subtract(arm_pot).abs();
         }
         
+        //sets previous location to current location for next time the method is called
         previous_arm_pot = arm_pot;
 
+        //returns true if the difference between the current arm difference and total difference is more than zero, checks if the arm moved the amount it needed to move
         return endAngleDifference.compareTo(this.angle)>=0;
     }
 
     @Override
     protected void end()
     {
+        //sets velocity of the arm to zero
         Robot.getArm().stop();
     }
 }
